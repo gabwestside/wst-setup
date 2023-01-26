76c3dc0aacd62559472329a2581484d35cbcac39
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-01-25 23:59:04 -0300

    closing initial configuration for backend"

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,22 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return 'Hello World'
+app.register(cors)
+
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server is running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server is running!')
+  })
