13bcb2076e406c12d1f6c594ab38580fe70eb2b3
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-02-06 13:01:16 -0300

    feat: finalizing initial structure of a mobile project

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts,
+} from '@expo-google-fonts/inter'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
 
-import { StatusBar } from 'expo-status-bar'
+import { Loading } from './src/components/Loading'
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  })
+
+  if (!fontLoaded) {
+    return <Loading />
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello world!</Text>
-      <StatusBar style='auto' />
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
     </View>
   )
 }
@@ -14,13 +36,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: 'purple',
+    color: '#7C3AED',
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    fontFamily: 'Inter_800ExtraBold',
   },
 })
