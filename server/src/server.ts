084d16a85069268cc7b325f1b2952f80aa4bad71
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-02-12 16:03:16 -0300

    feat: adding a migrations to databases and clean server file for reducer routes dependency

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,14 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes'
 import cors from '@fastify/cors'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/habits', async () => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server is running!')
 })
-
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('HTTP Server is running!')
-  })
