c4f35a18a823133a3bc8955039e5190db2aa79ad
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-02-15 00:27:59 -0300

    feat : adding phosphor icon lib, and create header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,18 @@
-import './styles/global.css'
+import './styles/global.css';
 
-import { Habit } from './components/Habit'
+import { Header } from './components/Header';
 
-function App() {
+// import { Habit } from './components/Habit'
+
+
+export function App() {
   return (
-    <div>
-      <Habit completed={1} />
-      <Habit completed={15} />
-      <Habit completed={10} />
-      <Habit completed={0} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        
+      <Header />
+        
+      </div>
     </div>
   )
 }
-
-export default App
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        background: '#09090A'
+      },
+    },
+    plugins: [],
+  }
 }
