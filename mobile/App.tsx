5bbb325110902b319488124c43403d2534d05be7
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-04-20 00:22:21 -0300

    feat: adding home screen and header component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,9 +5,10 @@ import {
   Inter_800ExtraBold,
   useFonts,
 } from '@expo-google-fonts/inter'
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'react-native'
 
 import { Loading } from './src/components/Loading'
+import { Home } from './src/screens/Home'
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -22,28 +23,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world!</Text>
+    <>
+      <Home />
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-    </View>
+    </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#7C3AED',
-    fontSize: 20,
-    fontWeight: 'bold',
-    fontFamily: 'Inter_800ExtraBold',
-  },
-})
