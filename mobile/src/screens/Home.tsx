a5c7f258d51a867f9208ddfc3139b07f77a4b180
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-04-22 21:38:23 -0300

    fix: removing console log

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -10,9 +10,7 @@ const datesFromYearStart = generateDatesFromYearBeginning()
 const minimumSummaryDatesSizes = 18 * 5
 const amountOfDaysToFill = minimumSummaryDatesSizes + datesFromYearStart.length
 
-export function Home() {
-  console.log(amountOfDaysToFill);
-  
+export function Home() {  
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
