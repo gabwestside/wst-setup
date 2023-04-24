04d26b3cfc3004e2013b02861cea7fd689fb69f2
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-04-23 23:56:59 -0300

    feat: creating habit method to complete or not a habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -70,4 +70,31 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits,
     }
   })
+
+  // complete or not complete a habit
+  app.patch('/habits/:id/toggle', async (request) => {
+    // route param => identity parameter
+
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleHabitParams.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      })
+    }
+  })
 }
