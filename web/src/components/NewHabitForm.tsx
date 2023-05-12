e2ff91bd6df0186710c7a8fecc800ae21a7fa18e
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-12 00:08:54 -0300

    feat: building the form modal component

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,7 +1,29 @@
+import { Check } from "phosphor-react";
+
 interface NewHabitFormProps {}
 
 export default function NewHabitForm(props: NewHabitFormProps) {
   return (
-    <div>Hello</div>
+    <form className='w-full flex flex-col mt-6'>
+      <label htmlFor='title' >
+        What's your commitment
+      </label>
+
+      <input 
+        type='text'
+        id='title'
+        placeholder="ex.: Exercises, sleep well, etc..."
+        autoFocus
+      />
+
+      <label htmlFor="">
+        What's the recurrence?
+      </label>
+
+      <button>
+        <Check size={20} weight='bold' />
+        Confirm
+      </button>
+    </form>
   )
 }
