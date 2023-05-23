75a70cddaff3bf0d007aac3e566879bb2c53022a
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-22 23:22:45 -0300

    feat: creating a progress bar for popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,9 +1,27 @@
+import * as Popover from '@radix-ui/react-popover'
+import { ProgressBar } from './ProgressBar'
+
 interface HabitDayProps {
   completed?: number
 }
 
 export function HabitDay({ completed }: HabitDayProps) {
   return (
-    <div className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg'></div>
+    <Popover.Root>
+      <Popover.Trigger className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg' />
+
+      <Popover.Portal>
+        <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-800 flex flex-col'>
+          <span className='font-semibold text-zinc-400'>Tuesday</span>
+          <span className='mt-1 font-extrabold leading-tight text-3xl'>
+            17/06
+          </span>
+
+          <ProgressBar progress={40} />
+
+          <Popover.Arrow height={8} width={16} className='fill-zinc-800' />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
