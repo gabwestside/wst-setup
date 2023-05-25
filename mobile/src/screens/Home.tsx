6d72c3084dcd8873cb1f414a06a3cd65c8716790
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-25 00:32:50 -0300

    refactor: adding navigate for habit screen

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -3,6 +3,7 @@ import { View, Text, ScrollView } from 'react-native'
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning.ts'
 
 import { Header } from '../components/Header'
+import { useNavigation } from '@react-navigation/native'
 import { HabitDay, DAY_SIZE } from '../components/HabitDay'
 
 const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
@@ -11,6 +12,8 @@ const minimumSummaryDatesSizes = 18 * 5
 const amountOfDaysToFill = minimumSummaryDatesSizes + datesFromYearStart.length
 
 export function Home() {  
+  const { navigate } = useNavigation()
+
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
@@ -36,6 +39,7 @@ export function Home() {
             datesFromYearStart.map(date => (
               <HabitDay
                 key={date.toISOString()}
+                onPress={() => navigate('habit', { date: date.toISOString() })}
               />
             ))
           }
