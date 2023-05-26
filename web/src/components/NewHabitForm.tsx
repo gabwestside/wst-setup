7f198400a70067bdc2e94bbcd6746b39d5d03b9a
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-25 23:04:47 -0300

    feat: adding checkbox ate newHabit form

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,4 +1,15 @@
 import { Check } from 'phosphor-react'
+import * as Checkbox from '@radix-ui/react-checkbox'
+
+const availableWeekDays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
 
 interface NewHabitFormProps {}
 
@@ -21,6 +32,21 @@ export default function NewHabitForm(props: NewHabitFormProps) {
         What's the recurrence?
       </label>
 
+      <div className='flex flex-col gap-2 mt-3'>
+        {availableWeekDays.map((day) => {
+          return (
+            <Checkbox.Root key={day} className='flex items-center gap-3 group'>
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-700 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                <Checkbox.Indicator>
+                  <Check size={20} className='text-whit' />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className='text-white leading-tight'>{day}</span>
+            </Checkbox.Root>
+          )
+        })}
+      </div>
       <button
         type='submit'
         className='mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 transition duration-300 ease-out hover:bg-green-500 hover:ease-in'
