0185118c5a961c590bd55d51be1ff8364d419d2e
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-06-04 23:58:39 -0300

    refactor: trying to connect with API base

--- mobile/src/lib/axios.ts
+++ mobile/src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.0.92:3333'
+  baseURL: 'http://localhost:3333'
+  // baseURL: 'http://192.168.0.92:3333'
 })
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -2,28 +2,29 @@ import { View, Text, ScrollView, Alert } from 'react-native'
 
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning.ts'
 
-import { api } from '../lib/axios';
+import { api } from '../lib/axios'
 import { Header } from '../components/Header'
 import { useNavigation } from '@react-navigation/native'
 import { HabitDay, DAY_SIZE } from '../components/HabitDay'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
+import { Loading } from '../components/Loading'
 
 const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 const datesFromYearStart = generateDatesFromYearBeginning()
 const minimumSummaryDatesSizes = 18 * 5
 const amountOfDaysToFill = minimumSummaryDatesSizes + datesFromYearStart.length
 
-export function Home() {  
+export function Home() {
   const [loading, setLoading] = useState(true)
   const [summary, setSummary] = useState(null)
 
-  const { navigate } = useNavigation() 
+  const { navigate } = useNavigation()
 
   async function fetchData() {
     try {
       setLoading(true)
-      const response = await api.get('summary')
-      console.log(response.data)
+      const response = await api.get('/summary')
+      console.log('2123123', response)
       setSummary(response.data)
     } catch (error) {
       Alert.alert('Oops', 'Unable to load habit summary.')
@@ -36,6 +37,10 @@ export function Home() {
   useEffect(() => {
     fetchData()
   }, [])
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
@@ -58,26 +63,21 @@ export function Home() {
         contentContainerStyle={{ paddingBottom: 100 }}
       >
         <View className='flex-row flex-wrap'>
-          {
-            datesFromYearStart.map(date => (
-              <HabitDay
-                key={date.toISOString()}
-                onPress={() => navigate('habit', { date: date.toISOString() })}
-              />
-            ))
-          }
+          {datesFromYearStart.map((date) => (
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate('habit', { date: date.toISOString() })}
+            />
+          ))}
 
-          {
-            amountOfDaysToFill > 0 && Array
-              .from({ length: amountOfDaysToFill })
-              .map((_, index) => (
-                <View
-                  key={index}
-                  className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-700 opacity-40'
-                  style={{ width: DAY_SIZE, height: DAY_SIZE }}
-                />
-              ))
-          }
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                key={index}
+                className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-700 opacity-40'
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
         </View>
       </ScrollView>
     </View>
