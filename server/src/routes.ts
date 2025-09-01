05efc225e657e493f7cec1a444a56690c998e4a0
Gabriel Moura <gabriel.lima@aspec.com.br>
2025-09-01 16:54:42 -0300

    feat: :fire: update api endpoint of toggle mark a past habit as completed

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -53,28 +53,36 @@ export async function appRoutes(app: FastifyInstance) {
   })
 
   // complete or not complete a habit
-  app.patch('/habits/:id/toggle', async (request) => {
-    const toggleHabitParams = z.object({ id: z.string().uuid() })
-    const { id } = toggleHabitParams.parse(request.params)
-
-    const today = dayjs.utc().startOf('day').toDate() // 👈 UTC
-
-    let day = await prisma.day.findUnique({ where: { date: today } })
-    if (!day) {
-      day = await prisma.day.create({ data: { date: today } })
-    }
-
-    const dayHabit = await prisma.dayHabit.findUnique({
-      where: { day_id_habit_id: { day_id: day.id, habit_id: id } },
-    })
-
-    if (dayHabit) {
-      await prisma.dayHabit.delete({ where: { id: dayHabit.id } })
-    } else {
-      await prisma.dayHabit.create({ data: { day_id: day.id, habit_id: id } })
-    }
+app.patch('/habits/:id/toggle', async (request) => {
+  const toggleHabitParams = z.object({ id: z.string().uuid() })
+  const toggleHabitQuery = z.object({
+    date: z.coerce.date().optional(), // <- optional
   })
 
+  const { id } = toggleHabitParams.parse(request.params)
+  const { date } = toggleHabitQuery.parse(request.query)
+
+  // toggle target = date received (UTC) or today (UTC)
+  const targetDay = dayjs.utc(date ?? new Date()).startOf('day').toDate()
+
+  let day = await prisma.day.findUnique({ where: { date: targetDay } })
+
+  if (!day) {
+    day = await prisma.day.create({ data: { date: targetDay } })
+  }
+  
+  const dayHabit = await prisma.dayHabit.findUnique({
+    where: { day_id_habit_id: { day_id: day.id, habit_id: id } },
+  })
+
+  if (dayHabit) {
+    await prisma.dayHabit.delete({ where: { id: dayHabit.id } })
+  } else {
+    await prisma.dayHabit.create({ data: { day_id: day.id, habit_id: id } })
+  }
+
+  return { ok: true }
+})
   app.get('/summary', async () => {
     // versão Postgres já corrigida
     const summary = await prisma.$queryRaw<
